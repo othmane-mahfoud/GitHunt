d3af4a6add719f9f94a9b2064e5bdb6a5a1b72f6
Othmane <mahfoud.othmane97@gmail.com>
2018-06-21 22:13:44 +0100

    Initial Commit

---
+++ GitHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHunt
+//
+//  Created by Othmane on 6/21/18.
+//  Copyright © 2018 Othmane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
