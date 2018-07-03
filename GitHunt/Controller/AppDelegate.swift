81f4905a7b86340dce9c4e481ed7998b7947e285
Othmane <mahfoud.othmane97@gmail.com>
2018-07-03 14:10:15 +0100

    Login with github working

--- GitHunt/Controller/AppDelegate.swift
+++ GitHunt/Controller/AppDelegate.swift
@@ -28,6 +28,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        if "ppoauthapp" == url.scheme || (url.scheme?.hasPrefix("com.googleusercontent.apps"))! {
+            if let vc = window?.rootViewController as? SignInViewController {
+                vc.oauth2.handleRedirectURL(url)
+                return true
+            }
+        }
+        return false
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- GitHunt/Controller/SignInViewController.swift
+++ GitHunt/Controller/SignInViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 import Firebase
+import Alamofire
+import p2_OAuth2
 
 class SignInViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    fileprivate var alamofireManager: SessionManager?
+    
+    var loader: OAuth2DataLoader?
+    
+    var oauth2 = OAuth2CodeGrant(settings: [
+        "client_id": "16e2b1b101f77b9ce07e",                         // yes, this client-id and secret will work!
+        "client_secret": "110ebfed3aa74fe3e1ad8fc52b56910fbe42fe0c",
+        "authorize_uri": "https://github.com/login/oauth/authorize",
+        "token_uri": "https://github.com/login/oauth/access_token",
+        "scope": "user repo:status",
+        "redirect_uris": ["ppoauthapp://oauth/callback"],            // app has registered this scheme
+        "secret_in_body": true,                                      // GitHub does not accept client secret in the Authorization header
+        "verbose": true,
+        ] as OAuth2JSON)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,8 +63,49 @@ class SignInViewController: UIViewController {
     
     @IBAction func githubSignInPressed(_ sender: Any) {
         
-        self.performSegue(withIdentifier: "goToRepoList", sender: self)
+        if oauth2.isAuthorizing {
+            oauth2.abortAuthorization()
+            return
+        }
+        oauth2.authConfig.authorizeEmbedded = false
+        let loader = OAuth2DataLoader(oauth2: oauth2)
+        self.loader = loader
         
+        loader.perform(request: userDataRequest) { response in
+            do {
+                let json = try response.responseJSON()
+                self.didGetUserdata(dict: json, loader: loader)
+            }
+            catch let error {
+                self.didCancelOrFail(error)
+            }
+        }
+        
+    }
+    
+    var userDataRequest: URLRequest {
+        var request = URLRequest(url: URL(string: "https://api.github.com/user")!)
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        return request
+    }
+    
+    func didGetUserdata(dict: [String: Any], loader: OAuth2DataLoader?) {
+        DispatchQueue.main.async {
+            if let username = dict["name"] as? String {
+                print(username)
+            }
+            else{
+                print("here")
+            }
+        }
+    }
+    
+    func didCancelOrFail(_ error: Error?) {
+        DispatchQueue.main.async {
+            if let error = error {
+                print("Authorization went wrong: \(error)")
+            }
+        }
     }
     
     
